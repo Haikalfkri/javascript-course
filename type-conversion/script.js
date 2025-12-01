73a2f8962bc1421ded4554cefbc612705ede76ee
Haikal Fikri <haikaljr1245@gmail.com>
2025-12-02 05:24:06 +0700

    type conversion

---
+++ type-conversion/script.js
@@ -0,0 +1,21 @@
+// type conversion = change the datatype of a value to another data type
+
+// let age = prompt("Enter your age: ");
+// age = Number(age);
+
+// age += 1;
+
+// console.log(age, typeof age);
+
+
+let x;
+let y;
+let z;
+
+x = String(x);
+y = Boolean(y);
+z = Number(z);
+
+console.log(x, typeof x);
+console.log(y, typeof y);
+console.log(z, typeof z);
