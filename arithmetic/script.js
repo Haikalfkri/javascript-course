b540a1922ba174d39006d409c4dcc2ebec1a0552
Haikal Fikri <haikaljr1245@gmail.com>
2025-12-02 05:08:51 +0700

    arithmetic operators

---
+++ arithmetic/script.js
@@ -0,0 +1,27 @@
+// arithmetic operators = operands (values, variabels, etc) and operator (+, -, *, /, %)
+
+let students = 41;
+
+// students = students + 5;
+// students = students - 4;
+// students = students * 2;
+// students = students / 2;
+// students = students ** 2;
+// students = students % 4;
+
+// augmented assignment operators
+// students += 5;
+// students -= 4;
+// students *= 2;
+// students /= 2;
+// students **= 2;
+// students %= 3;
+
+// increment and decrement operators
+// students++;
+// students--;
+
+
+let result = 2 * 3 + 4 - 8 / (2 - 1);
+
+console.log(result);
