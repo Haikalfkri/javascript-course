5002fce9e5ab284ff72203e8a7386a302b28bfb0
Haikal Fikri <haikaljr1245@gmail.com>
2025-12-02 04:39:41 +0700

    Starter

---
+++ starter/index.js
@@ -0,0 +1,16 @@
+// console.log("Hello, World!");
+// console.log("I like burger");
+
+// this is the comment
+/**
+ * this is
+ * a
+ * multi-line
+ * comment
+ */
+
+// add alert
+// window.alert("This is an alert message!");
+
+document.getElementById("h1").textContent = "Hello, World!";
+document.getElementById("p").textContent = "I like burget.";
